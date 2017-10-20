5fec58933cd81a5af2c123168cf1cad59b9a17d8
galian <wanglc@hotmail.com>
2017-10-21 01:46:25 +0800

    first add

---
+++ mylib/src/main/java/com/galian/mylib/Utils.java
@@ -0,0 +1,46 @@
+package com.galian.mylib;
+
+import android.content.pm.IPackageManager;
+import android.os.RemoteException;
+import android.util.Log;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+
+/**
+ * Created by galian on 2017/10/3.
+ */
+
+public class Utils {
+
+    private static final String TAG = "Utils";
+    public static boolean isProtectedBroatcast(String action) {
+        Class<?> cls = null;
+        Method method = null;
+        boolean ret = false;
+
+        try {
+            cls = Class.forName("android.app.ActivityThread");
+            method = cls.getMethod("getPackageManager", null);
+            if (!method.isAccessible()) {
+                method.setAccessible(true);
+            }
+            IPackageManager iPackageManager = (IPackageManager) method.invoke(cls, null);
+            ret = iPackageManager.isProtectedBroadcast(action);
+            Log.d(TAG, "action: " + action + " is " + (ret?"protected.":" not protected."));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+
+        return ret;
+    }
+}
